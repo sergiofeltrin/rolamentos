8929c58b2fd2f1147f27a9d942e6ead411a70076
sergiofeltrin <sergiofeltrin@users.noreply.github.com>
2026-04-21 15:08:04 +0000

    deploy: 47c9f502cbd3319db8859a0e5496cbff70359fd7

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'check-rolamento-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './index.css',
+    './index.js',
+    './manifest.json',
+    './logo_app.png',
+    './jspdf.umd.min.js',
+    './html2canvas.min.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
+});
